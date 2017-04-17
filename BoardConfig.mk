1e74a2974eaa33a05017e1c0ee63640f36bc45d5
Kasual <mushroomhead52e@gmail.com>
2017-04-17 18:49:05 -0400

    Update the repo properly with the right TWRP variables. Include the correct fstab.
    Change-Id: Iba35bf124faf2e5fec187ff4c298aef5fb1fff43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,15 +38,27 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/lge/ls990/rootdir/etc/fstab.g3
-
-# RIL
-#BOARD_RIL_CLASS += ../../../device/lge/ls990/ril/
+TARGET_RECOVERY_FSTAB := device/lge/ls990/rootdir/etc/twrp.fstab
 
 # RIL
 BOARD_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_10
 BOARD_GLOBAL_CPPFLAGS += -DUSE_RIL_VERSION_10
 TARGET_RIL_VARIANT := caf
+
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_USB_STORAGE := true
+TW_INCLUDE_CRYPTO := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
 
 # NFC
 BOARD_NFC_CHIPSET := pn544
@@ -66,3 +78,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/lge/ls990/BoardConfigVendor.mk
+
